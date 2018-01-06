ac108a5271e4e99e74e10a1ea40b77891b4d632e
CraftedCart <CraftedCart@users.noreply.github.com>
2018-01-06 19:27:10 +0000

    Animated the GDQ logo

--- source/GdqRender.cpp
+++ source/GdqRender.cpp
@@ -19,9 +19,22 @@ namespace GdqRender {
         gdqLogo->meshNormals = MODEL_NORMS;
         gdqLogo->triangleCount = 6616;
         gdqLogo->getTransform().pos.z = -50.0f;
+        gdqLogo->getTransform().scl = {0.0f, 0.0f, 0.0f};
         rootNode->addChild(gdqLogo);
 
         while (1) {
+            gdqLogo->getTransform().rot.y += 0.025;
+
+            if (gdqLogo->getTransform().scl.x < 1.0f) {
+                gdqLogo->getTransform().scl.x += 0.01f;
+                gdqLogo->getTransform().scl.y += 0.01f;
+                gdqLogo->getTransform().scl.z += 0.01f;
+            } else {
+                gdqLogo->getTransform().scl.x = 1.0f;
+                gdqLogo->getTransform().scl.y = 1.0f;
+                gdqLogo->getTransform().scl.z = 1.0f;
+            }
+
             RenderManager::draw(rootNode);
 
             PAD_Read(pads);
