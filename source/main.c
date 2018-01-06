18b7e665180d4df454992789b7d953f56d115e23
CraftedCart <CraftedCart@users.noreply.github.com>
2018-01-06 15:43:18 +0000

    Added normals, lighting, and changed vertex coloring (Which doesn't work with lighing)

--- source/main.c
+++ source/main.c
@@ -1,5 +1,6 @@
 #include "verts.h"
 #include "colors.h"
+#include "norms.h"
 #include <stdlib.h>
 #include <string.h>
 #include <malloc.h>
@@ -9,12 +10,19 @@
 GXRModeObj *screenMode;
 static void *frameBuffer;
 static vu8 readyForCopy;
-#define	FIFO_SIZE (256*1024)
+#define FIFO_SIZE (256*1024)
 
 void update_screen(Mtx viewMatrix);
 static void copy_buffers(u32 unused);
+void setLight(Mtx view, guVector lightPos, GXColor litcol, GXColor ambcol, GXColor matcol);
 
-int	main(void) {
+static GXColor litcolors[] = {
+        { 0xFF, 0x00, 0x00, 0xFF }, //Light color 1
+        { 0x40, 0x40, 0x40, 0xFF }, //Ambient 1
+        { 0xFF, 0xFF, 0xFF, 0xFF }  //Material 1
+};
+
+int main(void) {
     Mtx view;
     Mtx projection;
     PADStatus pads[4];
@@ -26,7 +34,7 @@ int	main(void) {
     screenMode = VIDEO_GetPreferredMode(NULL);
 
     PAD_Init();
-    frameBuffer	= MEM_K0_TO_K1(SYS_AllocateFramebuffer(screenMode));
+    frameBuffer = MEM_K0_TO_K1(SYS_AllocateFramebuffer(screenMode));
 
     VIDEO_Configure(screenMode);
     VIDEO_SetNextFramebuffer(frameBuffer);
@@ -35,7 +43,7 @@ int	main(void) {
     VIDEO_Flush();
 
     fifoBuffer = MEM_K0_TO_K1(memalign(32,FIFO_SIZE));
-    memset(fifoBuffer,	0, FIFO_SIZE);
+    memset(fifoBuffer, 0, FIFO_SIZE);
 
     GX_Init(fifoBuffer, FIFO_SIZE);
     GX_SetCopyClear(backgroundColor, 0x00ffffff);
@@ -63,18 +71,20 @@ int	main(void) {
     GX_ClearVtxDesc();
     GX_SetVtxDesc(GX_VA_POS, GX_INDEX16);
     GX_SetVtxDesc(GX_VA_CLR0, GX_INDEX16);
+    GX_SetVtxDesc(GX_VA_NRM, GX_INDEX16);
     GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_POS, GX_POS_XYZ, GX_F32, 0);
     GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_CLR0, GX_CLR_RGBA, GX_RGBA8, 0);
+    GX_SetVtxAttrFmt(GX_VTXFMT0, GX_VA_NRM, GX_NRM_XYZ, GX_F32, 0);
     GX_SetArray(GX_VA_POS, MODEL_VERTS, 3 * sizeof(f32));
     GX_SetArray(GX_VA_CLR0, MODEL_COLORS, 4 * sizeof(u8));
+    GX_SetArray(GX_VA_NRM, MODEL_NORMS, 3 * sizeof(f32));
     GX_SetNumChans(1);
     GX_SetNumTexGens(0);
     GX_SetTevOrder(GX_TEVSTAGE0, GX_TEXCOORDNULL, GX_TEXMAP_NULL, GX_COLOR0A0);
     GX_SetTevOp(GX_TEVSTAGE0, GX_PASSCLR);
 
-    while (1)
-    {
-        guLookAt(view, &camera,	&up, &look);
+    while (1) {
+        guLookAt(view, &camera, &up, &look);
         GX_SetViewport(0,0,screenMode->fbWidth,screenMode->efbHeight,0,1);
         GX_InvVtxCache();
         GX_InvalidateTexAll();
@@ -91,21 +101,28 @@ int	main(void) {
 void update_screen(Mtx viewMatrix) {
     Mtx modelView;
 
-    guMtxIdentity(modelView);
-    guMtxTransApply(modelView, modelView, 0.0F,	0.0F, -50.0F);
-    guMtxConcat(viewMatrix,modelView,modelView);
+    guLightPerspective(modelView, 60, 1.33f, 0.5f, 0.5f, 0.5f, 0.5f);
+    guVector lightPos = {0.0f, 0.0f, 5000.0f};
+    setLight(viewMatrix, lightPos, litcolors[0], litcolors[1], litcolors[2]);
 
-    GX_LoadPosMtxImm(modelView,	GX_PNMTX0);
+    guMtxIdentity(modelView);
+    guMtxTransApply(modelView, modelView, 0.0F, 0.0F, -50.0F);
+    guMtxConcat(viewMatrix, modelView, modelView);
+
+    GX_LoadPosMtxImm(modelView, GX_PNMTX0);
 
     GX_Begin(GX_TRIANGLES, GX_VTXFMT0, 6616 * 3);
 
     for (u16 i = 0; i < 6616 * 3; i += 3) {
         GX_Position1x16((u16) (i));
         GX_Color1x16((u16) (i));
+        GX_Normal1x16((u16) (i));
         GX_Position1x16((u16) (i + 1));
         GX_Color1x16((u16) (i + 1));
+        GX_Normal1x16((u16) (i + 1));
         GX_Position1x16((u16) (i + 2));
         GX_Color1x16((u16) (i + 2));
+        GX_Normal1x16((u16) (i + 2));
     }
 
     GX_End();
@@ -117,12 +134,31 @@ void update_screen(Mtx viewMatrix) {
     return;
 }
 
-static void	copy_buffers(u32 count __attribute__ ((unused))) {
+static void copy_buffers(u32 count __attribute__ ((unused))) {
     if (readyForCopy==GX_TRUE) {
-        GX_SetZMode(GX_TRUE, GX_LEQUAL,	GX_TRUE);
+        GX_SetZMode(GX_TRUE, GX_LEQUAL, GX_TRUE);
         GX_SetColorUpdate(GX_TRUE);
         GX_CopyDisp(frameBuffer,GX_TRUE);
         GX_Flush();
         readyForCopy = GX_FALSE;
     }
 }
+
+void setLight(Mtx view, guVector lightPos, GXColor litcol, GXColor ambcol, GXColor matcol) {
+    GXLightObj lobj;
+
+    guVecMultiply(view, &lightPos, &lightPos);
+
+    GX_InitLightPos(&lobj, lightPos.x, lightPos.y, lightPos.z);
+    /*GX_InitLightShininess*/
+    GX_InitLightColor(&lobj, litcol);
+    GX_LoadLightObj(&lobj, GX_LIGHT0);
+
+    //Set number of rasterized color channels
+    GX_SetNumChans(1);
+    GX_SetChanCtrl(GX_COLOR0, GX_ENABLE, GX_SRC_REG, GX_SRC_REG, GX_LIGHT0, GX_DF_CLAMP, GX_AF_NONE);
+
+    GX_SetChanAmbColor(GX_COLOR0, ambcol);
+    GX_SetChanMatColor(GX_COLOR0, matcol);
+}
+
