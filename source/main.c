93c9c2f2b691fa0589c8a741f9987d0e07692afa
CraftedCart <CraftedCart@users.noreply.github.com>
2018-01-06 12:50:50 +0000

    Moved camera backwards

--- source/main.c
+++ source/main.c
@@ -53,11 +53,11 @@ int	main(void) {
     GX_CopyDisp(frameBuffer,GX_TRUE);
     GX_SetDispCopyGamma(GX_GM_1_0);
 
-    guVector camera = {0.0F, 0.0F, 0.0F};
+    guVector camera = {0.0F, 0.0F, 50.0F};
     guVector up = {0.0F, 1.0F, 0.0F};
     guVector look = {0.0F, 0.0F, -1.0F};
 
-    guPerspective(projection, 60, 1.33F, 10.0F,	3000.0F);
+    guPerspective(projection, 60, 1.33F, 10.0F, 3000.0F);
     GX_LoadProjectionMtx(projection, GX_PERSPECTIVE);
 
     GX_ClearVtxDesc();
